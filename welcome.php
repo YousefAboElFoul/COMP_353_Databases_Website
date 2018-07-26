d5b93d774a50a597f5c20f1c8e4dc3a7cad8d932
Scabandari <ryan3nichols@gmail.com>
2018-07-26 14:16:53 -0400

    Login page works

--- welcome.php
+++ welcome.php
@@ -1,1 +1,36 @@
-<p>Welcome!</p>
+<?php
+// Initialize the session
+session_start();
+
+ // If session variable is not set it will redirect to login page
+//if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
+//    header("location: login.php");
+//    exit;
+//}
+
+if(!isset($_SESSION['username'])) {
+    echo "not isset";
+}
+if(empty($_SESSION['username'])) {
+    echo "empty";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+<div class="page-header">
+    <h1>Hi,  Welcome to our site.</h1>
+</div>
+<p><a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a></p>
+</body>
+</html>
