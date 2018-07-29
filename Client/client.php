90864991cbc5fe72b997f9243b943b25978d7c5a
YousefAboElFoul <yousefaboelfoul@gmail.com>
2018-07-29 17:40:50 -0400

    First set up for the client page

--- Client/client.php
+++ Client/client.php
@@ -1,1 +1,19 @@
+<html>
+<head>
+<title>
+Client Page
+</title>
+</head>
+<body>
+<?php
+echo "client will be able to see all active/expired contracts";
+echo "<br>";
+echo"\n\r clients will be able to provide my satifaction score";
+echo "<br>";
+echo"\n\r clients will be able to check satisfaction score of all the contracts managed by the manager"
 
+
+
+?>
+</body>
+</html>
