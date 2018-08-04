ef55757480864cebc6cdbfaf5eea6223b0a41d88
YousefAboElFoul <yousefaboelfoul@gmail.com>
2018-08-04 15:25:44 -0400

    Updating the bootstrap and checking for faults

--- Client/satisfaction.php
+++ Client/satisfaction.php
@@ -3,6 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Satisfaction Form</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 750px;
+            margin: 0 auto;
+        }
+    </style>
 </head>
 <body>
     <h2>Contact Us</h2>
