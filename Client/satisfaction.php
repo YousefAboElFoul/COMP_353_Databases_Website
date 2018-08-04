e96f19919b98fc4eb2f4fa0f9acc20d3898bf350
YousefAboElFoul <yousefaboelfoul@gmail.com>
2018-08-04 15:58:25 -0400

    cleaned up alot of stuff and merged client to satisfaction

--- Client/satisfaction.php
+++ Client/satisfaction.php
@@ -1,5 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+// Initialize the session
+session_start();
+// If session variable is not set it will redirect to login page
+if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
+    header("location: login.php");
+    exit;
+}
+?>
 <head>
     <meta charset="UTF-8">
     <title>Satisfaction Form</title>
@@ -14,25 +23,39 @@
 <body>
     <h2>Contact Us</h2>
     <p>Please fill in your satsifaction rated between (1-10) and send us.</p>
-    <form action="client.php" method="post">
+    <form action="" method="post">
         <p>
             <label for="inputnumber">Satisfaction:<sup>*</sup></label>
-            <input type="number_format" name="name" id="inputnumber">
+            <input type="number_format" name="Satisfaction_num" id="inputnumber">
         </p>
-      <!--     <p>
-            <label for="inputEmail">Email:<sup>*</sup></label>
-            <input type="text" name="email" id="inputEmail">
-        </p>
-        <p>
-            <label for="inputSubject">Subject:</label>
-            <input type="text" name="subject" id="inputSubject">
-        </p>
-        <p>
-         <label for="inputComment">Message:<sup>*</sup></label>
-            <textarea name="message" id="inputComment" rows="5" cols="30"></textarea>
-        </p> -->
+     
         <input type="submit" value="Submit">
         <input type="reset" value="Reset">
     </form>
+	<!--<li><em>Satisfaction:</em> <?php echo $_POST["name"]?></li>-->
+<?php
+require_once 'config.php';
+
+$UserId = $_POST['Satisfaction_num'];
+ 
+// Attempt insert query execution
+$sql ="INSERT INTO Client (client_satisfaction) VALUES('Satisfaction_num') WHERE client";
+if(mysqli_query($conn, $sql)){
+    echo "Records inserted successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+}
+ 
+// Close connection
+mysqli_close($conn);
+echo "client will be able to see all active/expired contracts";
+echo "<br>";
+echo"\n\r clients will be able to provide my satifaction score";
+echo "<br>";
+echo"\n\r clients will be able to check satisfaction score of all the contracts managed by the manager"
+
+
+
+?>
 </body>
 </html>
