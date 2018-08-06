e4e5ff898b7d6a6ca3d300606cfa2b38727a0520
Scabandari <ryan3nichols@gmail.com>
2018-08-06 12:50:58 -0400

    got registration page working for Manager

--- Manager/register.php
+++ Manager/register.php
@@ -1,5 +1,6 @@
 <?php
 // Include config file
+session_start();
 require_once '../config.php';
 
 // Define variables and initialize with empty values
@@ -15,9 +16,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     } else{
         // Prepare a select statement
 
-        $sql = "SELECT id FROM account WHERE username = ?";
+        $sql = "SELECT id FROM Account WHERE username = ?";
 
-        if($stmt = mysqli_prepare($conn, $sql)){
+        if($stmt = mysqli_prepare($conn, $sql)) {
+            echo "working 0";
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_username);
 
@@ -43,6 +45,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         mysqli_stmt_close($stmt);
     }
 
+    // Validate name
+    if(empty(trim($_POST["name"]))){
+        $name_err = "Please enter a username.";
+    } else {
+        $name = trim($_POST["name"]);
+    }
     // Validate password
     if(empty(trim($_POST['password']))){
         $password_err = "Please enter a password.";
@@ -64,23 +72,51 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     // Check input errors before inserting in database
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-        //$type=4;
+        //echo "no errors";
+        $type=3;
         // Prepare an insert statement
-        $sql = "INSERT INTO Account (username, password,account_type) 
-		VALUES (?, ?, 4)";
+        $sql = "INSERT INTO Account (username, password, account_type) 
+		VALUES (?, ?, 3)";
 
         if($stmt = mysqli_prepare($conn, $sql)){
+           // echo "working1";
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
 
             // Set paramesters
             $param_username = $username;
-            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            //$param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            $param_password = $password;
+           // $param3 = 3;
 
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
-                // Redirect to login page
-                header("location: login.php");
+                //echo "working V";
+                $sql = "SELECT * FROM Account WHERE username = ?";
+                if($stmt = mysqli_prepare($conn, $sql)) {
+                    mysqli_stmt_bind_param($stmt, "s", $param1);
+                    $param1 = $username;
+                    if(mysqli_stmt_execute($stmt)) {
+                        //echo "Working z";
+                        $result = mysqli_stmt_get_result($stmt);
+                        if(mysqli_num_rows($result) == 1){
+                            $row =  mysqli_fetch_assoc($result);
+                            $user_id = $row['id'];
+                            $sql = "INSERT INTO Manager(name, user_id) VALUES(?, ?)";
+                            if($stmt = mysqli_prepare($conn, $sql)){
+                                //echo "working2";
+                                mysqli_stmt_bind_param($stmt, "ss", $param1, $param2);
+                                $param1 = $name;
+                                $param2 = $user_id;
+                                if(mysqli_stmt_execute($stmt)){
+                                    header("location: login.php");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
             } else{
                 echo "Something went wrong. Please try again later.";
             }
@@ -92,5 +128,52 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     // Close connection
     mysqli_close($conn);
-}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+<div class="wrapper">
+    <h2>Sign Up</h2>
+    <p>Please fill this form to create an account.</p>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+            <label>Username</label>
+            <input type="text" name="username"class="form-control" value="<?php echo $username; ?>">
+            <span class="help-block"><?php echo $username_err; ?></span>
+        </div>
+
+        <div class="form-group <?php echo (!empty($name_err)) ? 'has-error' : ''; ?>">
+            <label>Name</label>
+            <input type="text" name="name"class="form-control" value="<?php echo $name; ?>">
+            <span class="help-block"><?php echo $name_err; ?></span>
+        </div>
+
+        <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+            <label>Password</label>
+            <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
+            <span class="help-block"><?php echo $password_err; ?></span>
+        </div>
+        <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+            <label>Confirm Password</label>
+            <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
+            <span class="help-block"><?php echo $confirm_password_err; ?></span>
+        </div>
+        <div class="form-group">
+            <input type="submit" class="btn btn-primary" value="Submit">
+            <input type="reset" class="btn btn-default" value="Reset">
+        </div>
+        <p>Already have an account? <a href="login.php">Login here</a>.</p>
+    </form>
+</div>
+</body>
+</html>
